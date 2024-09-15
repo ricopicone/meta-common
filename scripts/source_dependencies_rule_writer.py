ee0e5840117f6fee262c7b2a6759dbac36b22134
Rico Picone <rpicone@stmartin.edu>
2024-09-14 18:40:50 -0700

    source dependencies rule writer

---
+++ scripts/source_dependencies_rule_writer.py
@@ -0,0 +1,25 @@
+import json
+import os
+import pathlib
+import argparse
+
+# Parse command line arguments (input file source-dependencies.json and output file source_dependencies.mk)
+parser = argparse.ArgumentParser(description='Generate source dependencies rule file.')
+parser.add_argument('source_dependencies_json', type=pathlib.Path, help='source-dependencies.json file')
+parser.add_argument('source_dependencies_mk', type=pathlib.Path, help='source_dependencies.mk file')
+args = parser.parse_args()
+source_dependencies_json = args.source_dependencies_json
+source_dependencies_mk = args.source_dependencies_mk
+
+# Process source-dependencies.json into data dict
+with source_dependencies_json.open() as f:
+    data = json.load(f)
+
+# Write the source dependencies rule Makefile include file source_dependencies.mk
+with source_dependencies_mk.open('w') as f:
+    f.write('# Source dependencies\n')
+    for target, dep in data.items():
+        target_tex = f'($commondir)/versioned/{target}/index.tex'
+        target_html = f'($commondir)/versioned/{target}/index.html'
+        dependencies = f'source/{dep}/main.py source/{dep}/main.md'
+        f.write(f'{target_tex}: {dependencies}\n')
